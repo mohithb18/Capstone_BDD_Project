31c5b199a8a96dd6f628053e2b1bbab499ed9c2e
mohithb18 <bmohith79@gmail.com>
2025-09-08 18:34:24 +0530

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features/HealthCare.feature", glue = { "stepDefinitions",
-		"hooks" }, plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber.json",
+		"Hooks" }, plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
