861dc1d17975e75ce1c451d4585c9159cb50a9de
mohithb18 <bmohith79@gmail.com>
2025-09-08 16:54:33 +0530

    Initial Commit

---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,12 @@
+package testRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/Features/Capstone_HomePage.feature", glue = { "StepDefinations",
+		"Hooks" }, plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
